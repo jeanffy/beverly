d4496235972f0a632d497f9e7dedb966ec51b717
jeanffy <26540464+jeanffy@users.noreply.github.com>
2024-03-09 10:37:09 +0100

    feat: port forward on pod

--- src/render/renderers/pod.renderer.ts
+++ src/render/renderers/pod.renderer.ts
@@ -1,4 +1,4 @@
-import { V1PodList, V1PodSpec } from '@kubernetes/client-node';
+import { V1ContainerPort, V1PodList, V1PodSpec } from '@kubernetes/client-node';
 import { Renderer, Template } from '../base.renderer.js';
 import { Breadcrumb } from '../breadcrumb.js';
 import { Kubectl } from '../kubectl.js';
@@ -12,17 +12,32 @@ export class PodRenderer extends Renderer {
 
   public async render(podName: string): Promise<string> {
     const kubectl = new Kubectl(this.ctx);
+
     const pods = await kubectl.call<V1PodList>(`get pod --field-selector metadata.name=${podName}`);
     const pod = pods.items[0];
+
     const clusterArg = this.ctx?.clusterName !== undefined ? ` --cluster ${this.ctx.clusterName}` : '';
     const nsArg = this.ctx?.nsName !== undefined ? ` -n ${this.ctx.nsName}` : '';
+
     const terminalCmd = `kubectl exec -it ${podName}${clusterArg}${nsArg} -- sh`;
     const logsCmd = `kubectl logs -f ${podName}${clusterArg}${nsArg}`;
     const logsWithTailCmd = `kubectl logs --tail=100 -f ${podName}${clusterArg}${nsArg}`;
+
+    let portForwardCmd: string | undefined;
+    const podContainers = pod?.spec?.containers;
+    if (podContainers !== undefined && podContainers.length > 0) {
+      const podPorts = podContainers[0]?.ports;
+      if (podPorts !== undefined && podPorts.length > 0) {
+        const containerPort = (podPorts[0] as V1ContainerPort).containerPort;
+        portForwardCmd = `kubectl port-forward ${podName}${clusterArg}${nsArg} :${containerPort}`;
+      }
+    }
+
     if (pod !== undefined) {
       injectObjectRawProps(pod, ['metadata', 'spec', 'status']);
       injectObjectRawProps<V1PodSpec>(pod.spec!, ['containers', 'volumes']);
     }
+
     return this.renderTemplate(Template.Pod, {
       ...this.baseTemplateVars(),
       breadcrumb: new Breadcrumb(this.ctx).pod(podName),
@@ -30,6 +45,7 @@ export class PodRenderer extends Renderer {
       terminalCmd,
       logsCmd,
       logsWithTailCmd,
+      portForwardCmd,
     });
   }
 }
